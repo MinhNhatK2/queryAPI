7d5d437de0ffcca57bdf260c781ee87c112a736d
Nhat-Minh Ta <minh772k2@gmail.com>
2024-10-01 16:44:30 +0700

    upload

--- index.js
+++ index.js
@@ -395,7 +395,7 @@ UNION ALL
   });
 });
 // Chạy server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server chạy trên cổng ${PORT}`);
 });
